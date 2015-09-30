f7881ecdf988a297859922dedd9ba04e48e85982
Jonathan Muller <koren@pukogames.com>
2015-09-30 16:06:02 +0200

    Add minification tests, fix minificator and change return logic Now returns the minified object instead of modifying the reference

--- tests/minify-logic-spec.js
+++ tests/minify-logic-spec.js
@@ -6,20 +6,36 @@ function doneCallback(done){
   };
 }
 
+var item;
+
 describe('JSON Minifier minify logic', function() {
   it('should not minify object when no minification table provided', function(done){
-    var item =  { key: 'something' };
-    minifier.minify(item);
+    minifier.minify({ key: 'something' });
     expect(item.key).not.toBe(undefined);
     expect(item.key).toBe('something');
     done();
   });
 
   it('should not minify object inside object when no minification table provided', function(done){
-    var item =  { key: { secondLevel: 'something' } };
-    minifier.minify(item);
+    minifier.minify({ key: { secondLevel: 'something' } });
     expect(item.key.secondLevel).not.toBe(undefined);
     expect(item.key.secondLevel).toBe('something');
+    done();
+  });
+
+  it('should not minify object when minification table don\'t match the object', function(done){
+    minifier = require('../')({ key2: 'k' });
+    item = minifier.minify({ key: 'something' });
+    expect(item.key).not.toBe(undefined);
+    expect(item.key).toBe('something');
+    done();
+  });
+
+  it('should minify object when minification table match the object', function(done){
+    minifier = require('../')({ key2: 'k' });
+    item = minifier.minify({ key2: 'something' });
+    expect(item.k).not.toBe(undefined);
+    expect(item.k).toBe('something');
     done();
   });
 });
