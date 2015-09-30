e612d5ff85e09ab06d34dd4b5a9aa3aa4ae6516d
Jonathan Muller <koren@pukogames.com>
2015-09-30 16:07:48 +0200

    Fix tests

--- tests/minify-logic-spec.js
+++ tests/minify-logic-spec.js
@@ -10,14 +10,14 @@ var item;
 
 describe('JSON Minifier minify logic', function() {
   it('should not minify object when no minification table provided', function(done){
-    minifier.minify({ key: 'something' });
+    item = minifier.minify({ key: 'something' });
     expect(item.key).not.toBe(undefined);
     expect(item.key).toBe('something');
     done();
   });
 
   it('should not minify object inside object when no minification table provided', function(done){
-    minifier.minify({ key: { secondLevel: 'something' } });
+    item = minifier.minify({ key: { secondLevel: 'something' } });
     expect(item.key.secondLevel).not.toBe(undefined);
     expect(item.key.secondLevel).toBe('something');
     done();
