cca2cb447264cffbc1af74d89c734dd45ed27cc2
Jonathan Muller <koren@pukogames.com>
2015-09-30 13:06:33 +0200

    Add test for table parameter on entry point

--- tests/entry-point-spec.js
+++ tests/entry-point-spec.js
@@ -12,4 +12,10 @@ describe('JSON Minifier entry point', function() {
     expect(typeof minifier.minify).toBe('function');
     done();
   });
+
+  it('should expose table parameter', function(done){
+    expect(minifier.table).not.toBe(undefined);
+    expect(typeof minifier.table).toBe('object');
+    done();
+  });
 });
