1c63088566f4f80d76d095627912a8c4c245efca
Jonathan Muller <koren@pukogames.com>
2015-09-30 13:05:39 +0200

    Add test for exposed minify method and fix module inclusion in tests

--- tests/entry-point-spec.js
+++ tests/entry-point-spec.js
@@ -1,4 +1,4 @@
-var minifier = require('../');
+var minifier = require('../')();
 
 function doneCallback(done){
   return function(){
@@ -7,5 +7,9 @@ function doneCallback(done){
 }
 
 describe('JSON Minifier entry point', function() {
-
+  it('should expose minify function', function(done){
+    expect(minifier.minify).not.toBe(undefined);
+    expect(typeof minifier.minify).toBe('function');
+    done();
+  });
 });
